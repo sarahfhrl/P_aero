e21413463f6435d5a94c764772e1154d63e8b499
sarahfhrl <fuhrelsarah@orange.fr>
2024-11-23 17:23:52 +0100

    Add files via upload

---
+++ ABP2 Series 7000 Pa/Pression7000/Pression7000.ino
@@ -0,0 +1,34 @@
+#include "Honeywell_ABP.h"
+
+// create Honeywell_ABP instance
+// refer to datasheet for parameters
+Honeywell_ABP abp(
+    0x28,   // I2C address
+    0,      // minimum pressure
+    1,      // maximum pressure
+    "psi"   // pressure unit
+);
+
+void setup() {
+  // open serial communication
+  Serial.begin(9600);
+
+  // open I2C communication
+  Wire.begin();
+}
+
+void loop() {
+  // update sensor reading
+  abp.update();
+
+  // print sensor reading via serial
+  Serial.print("Pressure: ");
+  Serial.print(abp.pressure());
+  Serial.print(" ");
+  Serial.println(abp.unit());
+  Serial.print(" ");
+  Serial.print("Temperature: ");
+  Serial.print(abp.temperature());
+  Serial.println(" ºC ");
+  delay(500);
+}
